9e6cc620b27c313eeebbd2cc827de2f2094498ae
Todd Morningstar <todd@thoughtbot.com>
2019-10-29 16:58:30 -0700

    Modify Production Server Config 🎛

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path")
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,5 +41,14 @@ app.post("/api/talks", (req, res) => {
 
   res.send(newTalk);
 });
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
