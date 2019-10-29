f99c26efe445acb34b7163fe534e58433a9be6f6
Todd Morningstar <todd@thoughtbot.com>
2019-10-29 11:27:54 -0700

    Update App to Display some Lightning Talks ⚡

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    talks: [
+      {
+        id: 1,
+        title: "When Mystery Guests Become Stowaways"
+      },
+      {
+        id: 2,
+        title: "Top 10 Slide Animations for Designers"
+      }
+    ],
+  };
+
+  removeTalk = indexToRemove => {
+    const { talks } = this.state;
+
+    this.setState({
+      talks: talks.filter((_talk, index) => {
+        return index !== indexToRemove
+      })
+    });
+  };
+
+  handleSubmit = talk => {
+    this.setState({
+      talks: [...this.state.talks, talk]
+    });
+  };
+
+  render() {
+    const { talks } = this.state;
+    const talkListItems = talks.map((talk, index) => {
+      return <li key={talk.id} className="talk-list-item">{talk.title}</li>;
+    });
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://thoughtbot.com/san-francisco"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Visit thoughtbot</a>
+        </header>
+
+        <div className="container">
+          <ul className="talk-list">
+            {talkListItems}
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
