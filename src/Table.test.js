24394a812fd62fbd6a253f42a4b16294645d574d
Todd Morningstar <todd@thoughtbot.com>
2019-10-31 17:16:27 -0700

    Add Test for TableBody

--- src/Table.test.js
+++ src/Table.test.js
@@ -41,3 +41,46 @@ describe("TableHeader", () => {
     `);
   });
 });
+
+describe("TableBody", () => {
+  it("is expected to render link to show page, and button to delete", () => {
+    const talks = [
+      {
+        id: 12345,
+        title: "Top 10 Clickbait Titles in Test Suites"
+      },
+      {
+        id: 54321,
+        title: "Overcoming JavaScript Fatigue"
+      }
+    ];
+
+    const removeTalk = () => null;
+
+    act(() => {
+      render(
+        <MemoryRouter>
+          <table>
+            <TableBody talks={talks} removeTalk={removeTalk} />
+          </table>
+        </MemoryRouter>,
+        containerElement
+      );
+    });
+
+    expect(pretty(containerElement.innerHTML)).toMatchInlineSnapshot(`
+      "<table>
+        <tbody>
+          <tr class=\\"talk-table-row\\">
+            <td><a href=\\"/talks/12345\\">Top 10 Clickbait Titles in Test Suites</a></td>
+            <td><button><span role=\\"img\\" aria-label=\\"remove-talk\\">🗑</span></button></td>
+          </tr>
+          <tr class=\\"talk-table-row\\">
+            <td><a href=\\"/talks/54321\\">Overcoming JavaScript Fatigue</a></td>
+            <td><button><span role=\\"img\\" aria-label=\\"remove-talk\\">🗑</span></button></td>
+          </tr>
+        </tbody>
+      </table>"
+    `);
+  });
+});
